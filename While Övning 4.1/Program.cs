e8bcabb05f9cd82529ea4dfa0c8632739cb12660
Anton <>
2023-02-17 09:15:46 +0100

    Add project files.

---
+++ While Övning 4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string svar="0";
+            while (svar!="kina")
+            {
+                Console.WriteLine("Vilket är världens folkrikaste land?");
+                string svar1 = Console.ReadLine();
+                svar = svar1.ToLower();
+                if (svar != "kina")
+                    Console.WriteLine("FEL");
+                else
+                    Console.WriteLine("RÄTT");
+
+            }
+
+
+        }
+    }
+}
